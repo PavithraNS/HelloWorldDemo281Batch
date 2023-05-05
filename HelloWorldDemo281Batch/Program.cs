6d6bd0d6c4f242292767e75511bd8b15c334b019
PavithraNS <nspavi1991@gmail.com>
2023-05-05 12:58:32 +0530

    [Pavithra] Adding Human.cs

--- HelloWorldDemo281Batch/Program.cs
+++ HelloWorldDemo281Batch/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelloWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +12,22 @@ namespace HelloWorldDemo281Batch
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to RFP-281Batch");
-            Console.WriteLine("Modified in remote master branch");
-            Console.WriteLine("Creating conflict from remote");
-            Console.WriteLine("Modifying in local");
+            //Console.WriteLine("Modified in remote master branch");
+            //Console.WriteLine("Creating conflict from remote");
+            //Console.WriteLine("Modifying in local");
+            //object creation of human class
+            //className objectVaribel=new ClassName();
+            Human human = new Human();
+            Console.WriteLine("Please enter your name");
+            human.name = Console.ReadLine();
+            Console.WriteLine("Please enter your height");
+            //string height = Console.ReadLine();
+            human.height = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Please enter phone number");
+            //string height = Console.ReadLine();
+            human.phoneNumber = Convert.ToInt64(Console.ReadLine());
+            human.Talk();
+            human.HumanDetails();
             Console.ReadLine();
         }
     }
